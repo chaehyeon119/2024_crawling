8f98e47cca5f9bde31d9f69379e7f8d68441bc42
chaehyeon119 <kch971109@naver.com>
2024-02-01 22:54:10 +0900

    네이버 웹 크롤링 실습(view 탭 검색 결과 크롤링)

--- 01_naver.py
+++ 01_naver.py
@@ -15,47 +15,31 @@ headers = {
 }
 req = requests.get(url, headers=headers)
 
-
-# print(req.request.headers)
-
-# print(req.raise_for_status)
-# print(dir(req))
-# print(dir(req.request.headers))
-
-#네이버의 html이 출력됨
 html = req.text
 
-#print(html)
-
-#BeautifulSoup이라는 클래스를 사용해 html을 html.parser라는 것으로 분석을 한 것을 soup으로 부르겠다.
 soup = BeautifulSoup(html, "html.parser")
 
-# 클래스는 앞에 .을 붙이고, id는 앞에 #을 붙임
-#logo = soup.select_one(".imgsvg ico_naver")
 
-# 24.01.23. 현재 기준으로는 네이버 로고가 사라져서 none으로 뜸
-#
-#print(logo)
+# 게시물별 view 구역
+total_area = soup.select(".view_wrap")
 
-#클래스니까 점 붙이기, 빈칸은 .을 붙여 합치기
-titles = soup.select(".title_link_cross_trigger")
-# .title_link.cross_trigger가 아니라 위와 같이 되엉 ㅑ함 
+rank_num= 1
+for area in total_area:
+    
+    ad = area.select_one(".link_ad")
+    if ad:
+        # print("광고입니다.")
+        continue
+    print(f"<<<{rank_num}>>>")
 
-for title in titles:
-    print(title)
-    print('-----.text 붙인 것과 안 붙인 것의 차이')
-    print()
+    title = area.select_one(".title_area")
+    name = area.select_one(".name")
+    href = area.select_one(".title_link")
+
     print(title.text)
-    print("주소도 같이 가지고 오기['href']")
-    print(title['href'])
- 
-
-names = soup.select(".user_info")
-print(names)
-
-
-for result in zip(names, titles):
-    print(type(result))
-    print(result[0].text) #묶은 것 중에 첫번째
-    print(result[1].text) #묶은 것 중에 두번째
+    print(name.text)
+    # print(title["href"])
+    print(href["href"])
     print()
+    rank_num += 1
+    
